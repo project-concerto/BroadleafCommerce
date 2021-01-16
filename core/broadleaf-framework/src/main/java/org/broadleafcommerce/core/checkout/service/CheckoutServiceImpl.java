de49e02eef83314cfa0f8d70efbfb8f86df72088
project-concerto <xiaodongzhang97@gmail.com>
2021-01-16 16:51:54 +0800

    remove memory lock used in checkout

--- core/broadleaf-framework/src/main/java/org/broadleafcommerce/core/checkout/service/CheckoutServiceImpl.java
+++ core/broadleaf-framework/src/main/java/org/broadleafcommerce/core/checkout/service/CheckoutServiceImpl.java
@@ -35,8 +35,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import java.util.HashMap;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentMap;
 import javax.annotation.Resource;
 
 @Service("blCheckoutService")
@@ -51,21 +49,9 @@ public class CheckoutServiceImpl implements CheckoutService {
 
     @Resource(name="blOrderService")
     protected OrderService orderService;
-    
-    /**
-     * Map of locks for given order ids. This lock map ensures that only a single request can handle a particular order
-     * at a time
-     */
-    protected static ConcurrentMap<Long, Object> lockMap = new ConcurrentHashMap<>();
 
     @Override
     public CheckoutResponse performCheckout(Order order) throws CheckoutException {
-        //Immediately fail if another thread is currently attempting to check out the order
-        Object lockObject = putLock(order.getId());
-        if (lockObject != null) {
-            throw new CheckoutException("This order is already in the process of being submitted, unable to checkout order -- id: " + order.getId(), new CheckoutSeed(order, new HashMap<String, Object>()));
-        }
-
         // Immediately fail if this order has already been checked out previously
         if (hasOrderBeenCompleted(order)) {
             throw new CheckoutException("This order has already been submitted or cancelled, unable to checkout order -- id: " + order.getId(), new CheckoutSeed(order, new HashMap<String, Object>()));
@@ -94,9 +80,6 @@ public class CheckoutServiceImpl implements CheckoutService {
             throw new CheckoutException("Unable to checkout order -- id: " + order.getId(), e.getRootCause(), seed);
         } catch (RequiredAttributeNotProvidedException e) {
             throw new CheckoutException("Unable to checkout order -- id: " + order.getId(), e.getCause(), seed);
-        } finally {
-            // The order has completed processing, remove the order from the processing map
-            removeLock(order.getId());
         }
     }
     
@@ -108,26 +91,6 @@ public class CheckoutServiceImpl implements CheckoutService {
      */
     protected boolean hasOrderBeenCompleted(Order order) {
         return (OrderStatus.SUBMITTED.equals(order.getStatus()) || OrderStatus.CANCELLED.equals(order.getStatus()));
-    }
-
-    /**
-    * Get an object to lock on for the given order id
-    * 
-    * @param orderId
-    * @return null if there was not already a lock object available. If an object was already in the map, this will return
-    * that object, which means that there is already a thread attempting to go through the checkout workflow
-    */
-    protected Object putLock(Long orderId) {
-        return lockMap.putIfAbsent(orderId, new Object());
-    }
-    
-    /**
-     * Done with processing the given orderId, remove the lock from the map
-     * 
-     * @param orderId
-     */
-    protected void removeLock(Long orderId) {
-        lockMap.remove(orderId);
     }
     
 }
